17475bdc01302d5f6080a5fa62a5fe13101f0b39
metasloth <sam@trinityinsight.com>
2019-07-22 21:01:51 -0500

    removed extra spaces

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 class PConsole {
   /**
    * Create a new prefix logger
-   * @param {string} [prefix] - Immediately set's the logger's prefix 
+   * @param {string} [prefix] - Immediately set's the logger's prefix
    */
   constructor (prefix) {
     this.prefix = prefix || ''
   }
-  
+
   /**
    * Returns a reference to the logger for easy syntax
    * @returns {function(...string): void} - The logger function
@@ -15,15 +15,15 @@ class PConsole {
   getLogger () {
     return (...message) => this.log(...message)
   }
-  
+
   /**
    * Update the prefix used by the logger
    * @param {string} prefix - The prefix
    */
-  setPrefix(prefix) {
+  setPrefix (prefix) {
     this.prefix = prefix
   }
-  
+
   /**
    * Log a message with the configured prefix
    * @param  {...string} message - The message(s) to log
